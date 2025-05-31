e91d7fea91119e48cae01e4de78df2674ab93437
Glaymar Cueva <glaymarcueva02@gmail.com>
2025-06-01 02:26:23 +0800

    Initial commit1

--- Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda03622622d0a69103e32ac87b500a631aa86c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
